d489f40374d6ce098ac98c0e729532da87e04d90
Dihni Hassan <dihnihassan@gmail.com>
2023-01-02 16:23:58 -0500

    Create data for users and quizzes tables

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,5 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, password) VALUES ('Samreen', 123);
+INSERT INTO users (name, password) VALUES ('Hisham', 1234);
+INSERT INTO users (name, password) VALUES ('Dihni', 12345);
+
---
+++ db/seeds/02_quizzes.sql
@@ -0,0 +1,4 @@
+INSERT INTO quizzes (title, user_id, isPublic) VALUES ('Sports', 2, true);
+INSERT INTO quizzes (title, user_id, isPublic) VALUES ('Movies', 1, true);
+INSERT INTO quizzes (title, user_id, isPublic) VALUES ('Countries', 3, true);
+
