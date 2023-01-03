7dd1c6dee0dc10df7604a1bbcc47daf755a60a16
Dihni Hassan <dihnihassan@gmail.com>
2023-01-03 00:59:31 -0500

    Add questions and answers to quizzes

--- db/seeds/03_questions.sql
+++ db/seeds/03_questions.sql
@@ -3,16 +3,16 @@ INSERT INTO questions (quiz_id, question) VALUES (1, 'Which team won the 2019 NB
 INSERT INTO questions (quiz_id, question) VALUES (1, 'Which team won the MLB 2022 World Series?');
 INSERT INTO questions (quiz_id, question) VALUES (1, 'Which team won the 2022 FIFA World Cup?');
 INSERT INTO questions (quiz_id, question) VALUES (1, 'Which two jersey numbers did Michael Jordan wear?');
-INSERT INTO questions (quiz_id, question) VALUES (3, 'What is the capital of Canada?');
-INSERT INTO questions (quiz_id, question) VALUES (3, 'Which country is the largest in the world?');
-INSERT INTO questions (quiz_id, question) VALUES (3, 'What is the smallest country in the world?');
-INSERT INTO questions (quiz_id, question) VALUES (3, 'How many countries are in Africa?');
-INSERT INTO questions (quiz_id, question) VALUES (3, 'How many provinces are in Canada?');
 INSERT INTO questions (quiz_id, question) VALUES (2, 'Who did the cat in The Godfather belong to?');
 INSERT INTO questions (quiz_id, question) VALUES (2, 'How many Oscars has Denzel Washington won?');
 INSERT INTO questions (quiz_id, question) VALUES (2, 'How many Godfather movies are there?');
 INSERT INTO questions (quiz_id, question) VALUES (2, 'In which movie do Detectives Carter and Lee race to save a diplomats daughter?');
 INSERT INTO questions (quiz_id, question) VALUES (2, 'Who played the role of Malcolm X in the 1992 film of the same name?');
+INSERT INTO questions (quiz_id, question) VALUES (3, 'What is the capital of Canada?');
+INSERT INTO questions (quiz_id, question) VALUES (3, 'Which country is the largest in the world?');
+INSERT INTO questions (quiz_id, question) VALUES (3, 'What is the smallest country in the world?');
+INSERT INTO questions (quiz_id, question) VALUES (3, 'How many countries are in Africa?');
+INSERT INTO questions (quiz_id, question) VALUES (3, 'How many provinces are in Canada?');
 
 
 
--- db/seeds/04_answers.sql
+++ db/seeds/04_answers.sql
@@ -1,9 +1,55 @@
 INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, 'False', TRUE);
 INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, 'True', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, 'Toronto Raptors', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, 'Golden State Warriors', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, 'Houston Astros', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, 'Philadelphia Phillies', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, 'Argentina', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, 'France', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, '45 and 23', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, '22 and 56', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, 'Nobody, it was a stray cat', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, 'Don Corleone', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, '2', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, '0', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, '3', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, '5', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, 'Rush Hour', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, 'Rush Hour 2', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, 'Denzel Washington', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, 'Samuel L Jackson', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, 'Ottawa', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, 'Toronto', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, 'Russia', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, 'Canada', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, 'Vatican City', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, 'Malta', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, '54', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, '50', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, '10', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, '13', FALSE);
 
-INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, 'Ottawa', TRUE);
-INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, 'Toronto', FALSE);
-INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, 'Don Corleone', FALSE);
-INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, 'Nobody, it was a stray cat', TRUE);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
