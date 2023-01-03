ed28fd11e7290920bb67d60ceea63e9e20968886
Dihni Hassan <dihnihassan@gmail.com>
2023-01-02 21:15:55 -0500

    Add questions to questions table

--- db/seeds/04_answers.sql
+++ db/seeds/04_answers.sql
@@ -1,5 +1,6 @@
 INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, 'False', TRUE);
 INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, 'True', FALSE);
+
 INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, 'Ottawa', TRUE);
 INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, 'Toronto', FALSE);
 INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, 'Don Corleone', FALSE);
