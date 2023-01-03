a89cda246b16e83b4ed99b4dc706daf54095e175
ss-aa123 <neermas1@hotmail.com>
2023-01-03 16:17:56 -0500

    created login route and register route

---
+++ routes/login.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router  = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('login');
+});
+
+router.post('/', (req, res) => {
+  console.log('hello');
+  // validation
+});
+
+
+module.exports = router;
