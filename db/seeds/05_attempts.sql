4846029a5205cfe518dd7a54bbb6ddb51802ed89
Dihni Hassan <dihnihassan@gmail.com>
2023-01-03 01:04:25 -0500

    Add number of attempts and results

--- db/seeds/05_attempts.sql
+++ db/seeds/05_attempts.sql
@@ -1,4 +1,13 @@
 INSERT INTO attempts (user_id, quiz_id, result) VALUES (1, 2, 0);
 INSERT INTO attempts (user_id, quiz_id, result) VALUES (2, 1, 5);
-INSERT INTO attempts (user_id, quiz_id, result) VALUES (3, 3, 1);
+INSERT INTO attempts (user_id, quiz_id, result) VALUES (3, 3, 2);
+INSERT INTO attempts (user_id, quiz_id, result) VALUES (2, 1, 4);
+INSERT INTO attempts (user_id, quiz_id, result) VALUES (2, 3, 3);
+INSERT INTO attempts (user_id, quiz_id, result) VALUES (1, 2, 1);
+INSERT INTO attempts (user_id, quiz_id, result) VALUES (3, 1, 0);
+INSERT INTO attempts (user_id, quiz_id, result) VALUES (1, 1, 5);
+INSERT INTO attempts (user_id, quiz_id, result) VALUES (2, 3, 4);
+INSERT INTO attempts (user_id, quiz_id, result) VALUES (1, 2, 5);
+INSERT INTO attempts (user_id, quiz_id, result) VALUES (1, 1, 3);
+INSERT INTO attempts (user_id, quiz_id, result) VALUES (2, 2, 1);
 
