9cc2d208859889a357b8cd8ba183559f989ffd97
Dihni Hassan <dihnihassan@gmail.com>
2023-01-04 17:43:33 -0500

    update ejs and js files

--- routes/createQuiz.js
+++ routes/createQuiz.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router  = express.Router();
 
 router.get('/', (req, res) => {
+
+
   res.render('createQuiz');
 });
 
 router.post('/', (req, res) => {
   res.redirect('/');
-  console.log('hello');
+  // console.log('hello');
   // validation
 });
 
-
-module.exports = router;
