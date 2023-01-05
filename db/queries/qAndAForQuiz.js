18ff51641ab1701a6384c49d689b15f84f8639e7
HishamAlmoli <halmoli@my.centennialcollege.ca>
2023-01-04 19:47:39 -0500

    quizID parameter Added

--- db/queries/qAndAForQuiz.js
+++ db/queries/qAndAForQuiz.js
@@ -1,7 +1,7 @@
 const db = require('../connection');
 
 const getquizQA = (quizID) => {
-  return db.query('SELECT * FROM questions JOIN quizzes ON quiz_id = quizzes.id where quiz_id = $1;', [quizID])
+  return db.query('SELECT question, quiz_id, quizzes.title FROM questions JOIN quizzes ON quiz_id = quizzes.id JOIN answers ON questions.id = answers.question_id where quiz_id = $1;', [quizID])
 .then(data => {
       return data.rows;
     });
