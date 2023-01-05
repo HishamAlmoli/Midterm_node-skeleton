f46dcbea103f1a2eb5d8939efc09ebdc307546f4
HishamAlmoli <halmoli@my.centennialcollege.ca>
2023-01-05 16:29:03 -0500

    Add question query

--- db/queries/addQuestion.js
+++ db/queries/addQuestion.js
@@ -1,17 +1,36 @@
 const db = require('../connection');
 
-const addQuestion = (quizId, question, answer) => {
-  db.query(`INSERT INTO questions (quiz_id, question) VALUES ($1,$2);`, [quizId, question]);
-  const questionId = db.query(`select max(id) from questions;`);
-  let answer1 = true;
-  let answer2 = false;
-  if (answer === false) {
-    answer1 = false;
-    answer2 = true;
-  }
-  db.query(`INSERT INTO answers (question_id, answer, isCorrect) VALUES ($1, $2, $3);`, [questionId, 'True', answer1]);
-  db.query(`INSERT INTO answers (question_id, answer, isCorrect) VALUES ($1, $2, $3);`, [questionId, 'False', answer2]);
+const addQuestion = (quizId, question) => {
+  return db.query(`INSERT INTO questions (quiz_id, question) VALUES ($1,$2);`, [quizId, question]);
 
 };
+
+
+const addAnswer1 = (answer) => {
+  const max = db.query(`select max(id) from quizzes;`)
+  const questionId = db.query(`select max(id) from questions;`);
+  let answer1 = true;
+  if (answer === false) {
+    answer1 = false;
+  }
+  return db.query(`INSERT INTO answers (question_id, answer, isCorrect) VALUES ($1, $2, $3);`, [questionId, 'True', answer1]);
+};
+
+const addAnswer2 = (answer) => {
+  const max = db.query(`select max(id) from quizzes;`)
+  const questionId = db.query(`select max(id) from questions;`);
+  let answer2 = false;
+  if (answer === false) {
+    answer2 = true;
+  }
+  return db.query(`INSERT INTO answers (question_id, answer, isCorrect) VALUES ($1, $2, $3);`, [questionId, 'False', answer2]);
+};
+
+module.exports = {
+  addQuestion,
+  addAnswer1,
+  addAnswer2
+};
+
 
 module.exports = { addQuestion };
