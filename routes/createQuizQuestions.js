38a98302933b4202f831b6ef278826d59a4d922d
Dihni Hassan <dihnihassan@gmail.com>
2023-01-05 22:17:59 -0500

    Co-authored-by: Co-authored-by: Samreen Ahmed <ss-aa123@users.noreply.github.com> Co-authored-by: HishamAlmoli <HishamAlmoli@users.noreply.github.com>

--- routes/createQuizQuestions.js
+++ routes/createQuizQuestions.js
@@ -1,12 +1,43 @@
 const express = require('express');
 const router  = express.Router();
+const quizQueries = require('../db/queries/addQuestion');
 
 router.get('/', (req, res) => {
   res.render('createQuiz');
 });
 
 router.post('/', (req, res) => {
-  res.redirect('/');
+  console.log('QuizID ', req.body.QuizID);
+  console.log('req.body: ', req.body);
+  quizQueries.addQuestion(
+    req.body.QuizID,
+    req.body.question
+    )
+    .then(( ) => {
+      quizQueries.addAnswer1(
+        req.body.answer1,
+        req.body.check1
+        )
+        .catch((error ) => {
+          console.log('Answer1Error' + error)
+        });
+
+        quizQueries.addAnswer2(
+          req.body.answer2,
+          req.body.check2
+          )
+          .catch((error ) => {
+            console.log('Answer2Error' + error)
+          });
+
+    //console.log('idd '+ quizId);
+    })
+    .catch((error ) => {
+      console.log('AddQuestionError' + error)
+    });
+
+  res.render("createQuizQuestions", {quizId:req.body.QuizID});
+  //res.redirect('/');
   // console.log('hello');
   // validation
 });
