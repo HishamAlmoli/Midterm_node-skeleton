1d30ccdb306695aec9cd8ba8deb314aa2631130d
ss-aa123 <neermas1@hotmail.com>
2023-01-06 11:57:44 -0500

    final edits

--- db/queries/addQuestion.js
+++ db/queries/addQuestion.js
@@ -7,15 +7,17 @@ const addQuestion = (quizId, question) => {
 
 
 const addAnswer1 = (answer, isCorrect) => {
-  const max = db.query(`select max(id) from quizzes;`)
-  const questionId = db.query(`select max(id) from questions;`);
-  return db.query(`INSERT INTO answers (question_id, answer, isCorrect) VALUES ($1, $2, $3);`, [questionId, answer, isCorrect]);
+  // const max = db.query(select max(id) from quizzes;)
+  // const questionId = db.query(select max(id) from questions;);
+  // return db.query(INSERT INTO answers (question_id, answer, isCorrect) VALUES ($1, $2, $3);, [questionId, answer, isCorrect]);
+  return db.query(`INSERT INTO answers (question_id, answer, isCorrect) VALUES ((select max(id) from questions), $1, $2);`, [answer, isCorrect]);
 };
 
 const addAnswer2 = (answer, isCorrect) => {
-  const max = db.query(`select max(id) from quizzes;`)
-  const questionId = db.query(`select max(id) from questions;`);
-  return db.query(`INSERT INTO answers (question_id, answer, isCorrect) VALUES ($1, $2, $3);`, [questionId, answer, isCorrect]);
+  // const max = db.query(select max(id) from quizzes;)
+  // const questionId = db.query(select max(id) from questions;);
+  // return db.query(INSERT INTO answers (question_id, answer, isCorrect) VALUES ($1, $2, $3);, [questionId, answer, isCorrect]);
+  return db.query(`INSERT INTO answers (question_id, answer, isCorrect) VALUES ((select max(id) from questions), $1, $2);`, [answer, isCorrect]);
 };
 
 module.exports = {
